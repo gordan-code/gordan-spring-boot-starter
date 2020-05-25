2cc56342d7926723e73c381fa01bf1972c65bcf7
SHIGETA <gordanzhang@163.com>
2020-05-26 00:57:27 +0800

    Add files via upload

---
+++ gordan-spring-boot-starter/src/main/java/com/gordan/GordanSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.gordan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GordanSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GordanSpringBootStarterApplication.class, args);
+	}
+
+}
